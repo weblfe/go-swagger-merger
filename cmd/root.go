1e4d26c9c7ca1611263237a476299451b3f21c98
nuokwan <994685563@qq.com>
2023-02-09 21:22:42 +0800

    init repo

---
+++ cmd/root.go
@@ -0,0 +1,95 @@
+// Package cmd /*
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/weblfe/go-swagger-merger/swagger"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var outputFile string
+var cfgFile string
+var suffix = new([]string)
+var beautify bool
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "go-swagger-merger",
+	Short: "swagger merger",
+	Long:  `swagger merger`,
+	Example: `<files> go-swagger-merger -o swagger.json b.swagger.json c.swagger.json
+<dir> go-swagger-merger -o swagger.yaml api/bff/ 
+<suffix> go-swagger-merger -o swagger.json -s .swagger.json -s .swagger.yml  api/bff
+`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var (
+			merger = swagger.NewMerger()
+			filter = merger.CreatePatternFilter(*suffix)
+		)
+		for _, file := range args {
+			if err := merger.AddFile(file, filter); err != nil {
+				return err
+			}
+		}
+		return merger.Save(outputFile, beautify)
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-swagger-merger.
+	//yaml)")
+	defaultSuffix := []string{".swagger.json", ".swagger.yaml"}
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringVarP(&outputFile, "output", "o", "", "merge swagger.json to save file")
+	rootCmd.Flags().StringArrayVarP(suffix, "suffix", "s", defaultSuffix, "suffix filter for swagger matcher")
+	rootCmd.Flags().BoolVarP(&beautify, "beautify", "b", false, "merge swagger.json unzip beautify format")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".go-swagger-merger" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".go-swagger-merger")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ swagger/merge.go
@@ -0,0 +1,144 @@
+package swagger
+
+import (
+	"encoding/json"
+	"github.com/ghodss/yaml"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Merger struct {
+	Swagger map[string]any
+}
+
+func NewMerger() *Merger {
+	merger := new(Merger)
+	merger.Swagger = map[string]any{}
+	return merger
+}
+
+func (m *Merger) AddFile(file string, pattern ...func(string) bool) error {
+	stat, err := os.Stat(file)
+	if err != nil {
+		return err
+	}
+	if len(pattern) <= 0 {
+		pattern = append(pattern, func(s string) bool {
+			return strings.HasSuffix(s, "swagger.json")
+		})
+	}
+	if stat.IsDir() {
+		return filepath.Walk(file, m.recursive(file, pattern[0]))
+	}
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer func(f *os.File) {
+		err = f.Close()
+	}(f)
+
+	content, err := io.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	var s1 interface{}
+	err = yaml.Unmarshal(content, &s1)
+	if err != nil {
+		return err
+	}
+
+	return m.merge(s1.(map[string]any))
+}
+
+func (m *Merger) merge(f map[string]any) error {
+	for key, item := range f {
+		if i, ok := item.(map[string]interface{}); ok {
+			for subKey, subItem := range i {
+				if _, ok := m.Swagger[key]; !ok {
+					m.Swagger[key] = map[string]any{}
+				}
+
+				m.Swagger[key].(map[string]any)[subKey] = subItem
+			}
+		} else {
+			m.Swagger[key] = item
+		}
+	}
+	return nil
+}
+
+func (m *Merger) Save(fileName string, beautify ...bool) error {
+	var (
+		err error
+		res []byte
+		ext = strings.ToLower(strings.TrimPrefix(filepath.Ext(fileName), "."))
+	)
+	if len(beautify) <= 0 {
+		beautify = append(beautify, false)
+	}
+	switch ext {
+	case `json`, `json5`:
+		if !beautify[0] {
+			res, err = json.Marshal(m.Swagger)
+		} else {
+			res, err = json.MarshalIndent(m.Swagger, "", `  `)
+		}
+	case `yaml`, `yml`:
+		res, err = yaml.Marshal(m.Swagger)
+	}
+
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+
+	defer func(f *os.File) {
+		err = f.Close()
+	}(f)
+
+	_, err = f.Write(res)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 递归处理目录
+func (m *Merger) recursive(root string, pattern func(string) bool) filepath.WalkFunc {
+	root, _ = filepath.Abs(root)
+	return func(path string, info fs.FileInfo, err error) error {
+		if info == nil {
+			return nil
+		}
+		if !strings.HasPrefix(path, root) {
+			path = filepath.Join(root, path)
+		}
+		if info.IsDir() {
+			if root != path {
+				return filepath.Walk(path, m.recursive(path, pattern))
+			}
+			return nil
+		}
+		if !pattern(path) {
+			return nil
+		}
+		return m.AddFile(path)
+	}
+}
+
+func (m *Merger) CreatePatternFilter(suffix []string) func(string) bool {
+	return func(s string) bool {
+		for _, v := range suffix {
+			if strings.HasSuffix(s, v) {
+				return true
+			}
+		}
+		return false
+	}
+}
